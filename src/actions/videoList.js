30cbbc4f93a5e2b5d07d31359314f97953de5daa
bbissing <bbissing101@gmail.com>
2022-07-18 22:32:45 -0500

    end of Monday pair

--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,9 @@
+import VideoList from '../components/VideoList.js';
+
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+  type: 'CHANGE_VIDEO_LIST',
+  videos: videos
 });
 
 export default changeVideoList;
