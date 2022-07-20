8d41d6cfa5126ad7252c3e3f8bf40dcda437b4a6
bbissing <bbissing101@gmail.com>
2022-07-19 19:18:12 -0500

    "Complete defining action creators"

--- src/actions/search.js
+++ src/actions/search.js
@@ -6,8 +6,19 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 var handleVideoSearch = (q) => {
   //TODO:  Write an asynchronous action to handle a video search!
-  _.applyMiddleware(handleVideoSearch(q));
+  //_.applyMiddleware(handleVideoSearch(q));
+
+  return (dispatch) => {
+    searchYouTube({YOUTUBE_API_KEY, q}, (videos) => {
+      changeVideoList.videos = videos;
+      changeVideo.video = videos[0];
+    });
+  };
 
 };
 
 export default handleVideoSearch;
+
+
+//We could not use max = 5
+//Did we need to include the import's type?
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,3 @@
-import VideoList from '../components/VideoList.js';
-
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
   type: 'CHANGE_VIDEO_LIST',
